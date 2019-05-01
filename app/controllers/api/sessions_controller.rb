7c27c03343cc4303f02310e852485da84eb8e1f3
ndo277 <nhat.do277@gmail.com>
2019-04-30 17:30:50 -0700

    Build users/session contr. & users/show

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,21 @@
 class Api::SessionsController < ApplicationController
+  def create 
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user 
+      login(@user)
+      render :show
+    else 
+      render json: ["Invalid credentials"], status: 401
+    end
+  end
+
+  def destroy 
+    @user = current_user
+    if @user
+      logout
+      render {}
+    else
+      render json: ["Already signed out"], status: 404
+  end
+
 end
