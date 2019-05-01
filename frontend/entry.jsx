faabeea91bfeeb2f6b1679e2a38b7481287765f3
ndo277 <nhat.do277@gmail.com>
2019-05-01 09:59:00 -0700

    Add root/app components

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -9,5 +10,5 @@ document.addEventListener('DOMContentLoaded', () => {
   window.getState = store.getState;
   window.dispatch = store.dispatch;
 
-  ReactDOM.render(<h1>INSTACAM</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
