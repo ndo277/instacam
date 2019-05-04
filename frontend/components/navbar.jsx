b9bef62b9d29fdd10b27040bca818f12f2b77da1
ndo277 <nhat.do277@gmail.com>
2019-05-04 00:02:11 -0700

    Add link to home page on navbar-left click

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -4,16 +4,19 @@ const Navbar = (props) => {
   const personalGreeting = () => (
     <div className="navbar">
 
-        <div className="navbar-left">
+        <a className="navbar-left" href="/">
         <img className="logo-navbar" src="/images/logo-v.png" />
       <div className="navbar-pipe" />
       <h1 className="logo-name-navbar">Instacam</h1>
-        </div>
+        </a>
 
       <div className="navbar-spacer"/>
 
+        <div className="navbar-center">
+      <img className="profile-pic" src={props.currentUser.photoUrl} />
       <h3 className="welcome">Welcome, {props.currentUser.username}!</h3>
-      <img src={props.currentUser.photoUrl} alt=""/>
+        </div>
+
       <div className="navbar-spacer" />
       <button className="nav-button" onClick={props.logout}>Log Out</button>
     </div>
