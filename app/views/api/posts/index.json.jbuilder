d9f06bf49c6e663b312338fcb4f5303a10f29713
ndo277 <nhat.do277@gmail.com>
2019-05-06 15:16:56 -0700

    Add avatar to feed posts

--- app/views/api/posts/index.json.jbuilder
+++ app/views/api/posts/index.json.jbuilder
@@ -2,5 +2,7 @@
   json.set! post.id do
     json.extract! post, :id, :caption, :user_id
     json.photoUrl url_for(post.photo)
+    json.username post.user.username
+    json.avatarUrl url_for(post.user.avatar)
   end
 end
--- frontend/components/post_index_item.jsx
+++ frontend/components/post_index_item.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 const PostIndexItem = (props) => {
   return (
-    <div>
+    <div className="feed-post" >
+    
+      <div className="feed-post-top">
+        <img className="profile-pic" src={props.post.avatarUrl} />
+        <p className="avatar-name" >{props.post.username}</p>
+      </div>
+
       <img className= "feed-image" src={props.post.photoUrl}/>
     </div>
   )
