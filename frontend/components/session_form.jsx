51b0432052665e7bdafa59241a5137d5e9e96e9f
ndo277 <nhat.do277@gmail.com>
2019-05-10 11:41:31 -0700

    Pad session form typing cursor

--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -100,13 +100,13 @@ class SessionForm extends React.Component {
                   </div>
             
               <input className="form-field" type="text"
-                placeholder=" Username"
+                placeholder="Username"
                 value={this.state.username}
                 onChange={this.update('username')}
               />
 
             <input className="form-field" type="password"
-                placeholder=" Password"
+                placeholder="Password"
                 value={this.state.password}
                 onChange={this.update('password')}
               />
