fca692019644c07291fe5ac527e0176b09459fc6
ndo277 <nhat.do277@gmail.com>
2019-06-19 08:52:16 -0700

    Remove extraneous feed component

--- frontend/components/main_page.jsx
+++ frontend/components/main_page.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Feed from './feed';
 import SignupFormContainer from './signup_form_container';
+import PostIndexContainer from './post_index_container';
 
 const MainPage = (props) => {
   if (!props.currentUser) {
     return <SignupFormContainer />
   } else {
-    return <Feed />
+    return <PostIndexContainer />
   }
 }
 
