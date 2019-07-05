8d7a764050ee56668ed1b7b571b7d64fb25ac298
ndo277 <nhat.do277@gmail.com>
2019-07-05 04:39:51 -0700

    Fix validation typo

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,6 +1,6 @@
 class Following < ApplicationRecord
-  validates :follower_id, presence :true
-  validates :followee_id, presence :true, uniqueness: {scope: :follower_id}
+  validates :follower_id, presence: :true
+  validates :followee_id, presence: :true, uniqueness: {scope: :follower_id}
 
   
 end
