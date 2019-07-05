ff31375c237cef0d3edd4b927255a3fad2c91a32
ndo277 <nhat.do277@gmail.com>
2019-07-04 22:18:52 -0700

    Add and test index/create controller methods

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -4,6 +4,18 @@ class Api::FollowsController < ApplicationController
     render :index
   end
 
+  def create 
+    @follow = Follow.new(follow_params)
+    @follow.follower_id = current_user.id  
+    
+    if @follow.save 
+      render json: ['Follow created']
+    else
+      render json: @follow.errors.full_messages, status: 422
+    end
+    
+  end
+
   private
 
   def follow_params
