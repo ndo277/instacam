8aaab7f97e601055e21ed0f89d4240afb61b006a
ndo277 <nhat.do277@gmail.com>
2019-07-08 18:20:47 -0700

    Style buttons & list

--- frontend/components/following_index.jsx
+++ frontend/components/following_index.jsx
@@ -16,10 +16,14 @@ class FollowingIndex extends React.Component {
 
   handleFollowersClick(){
     this.setState({followTabDefault: true});
+    document.getElementById("following-button").style.color = "grey";
+    document.getElementById("follower-button").style.color = "black";
   }
 
   handleFollowingClick(){
     this.setState({followTabDefault: false});
+    document.getElementById("following-button").style.color = "black";
+    document.getElementById("follower-button").style.color = "grey";
   }
 
   render(){
@@ -33,13 +37,14 @@ class FollowingIndex extends React.Component {
         {this.props.currentUser.followers.map(flwr =>
           <li className="follow-index-list" key={flwr.id}>
             <div className="follow-item">
-              <img className="profile-pic" 
+              <img className="profile-pic-flw" 
                    onClick={() => this.props.history.push(`/users/${flwr.id}`)}
                    src={flwr.avatarUrl} 
                    alt="avatar" />
               <div className="flw-names" 
                    onClick={() => this.props.history.push(`/users/${flwr.id}`)}>
-                   {flwr.username} <br/> {flwr.display_name} 
+                  <div className="flw-username">{flwr.username}</div>
+                  <div className="flw-displayname">{flwr.display_name}</div>
               </div>
             </div>
           </li>
@@ -53,13 +58,14 @@ class FollowingIndex extends React.Component {
         this.props.currentUser.followees.map(flwe =>
           <li className="follow-index-list" key={flwe.id}>
             <div className="follow-item">
-              <img className="profile-pic" 
+              <img className="profile-pic-flw" 
                    src={flwe.avatarUrl} 
                    onClick={() => this.props.history.push(`/users/${flwe.id}`)}
                    alt="avatar" />
               <div className="flw-names" 
                    onClick={() => this.props.history.push(`/users/${flwe.id}`)}>
-                   {flwe.username} <br/> {flwe.display_name} 
+                <div className="flw-username">{flwe.username}</div>
+                <div className="flw-displayname">{flwe.display_name}</div>
               </div>
             </div>
           </li>
@@ -71,9 +77,15 @@ class FollowingIndex extends React.Component {
 
     return(
       <ul className="follow-index">
-        <button onClick={this.handleFollowersClick}>FOLLOWERS</button> 
-        <button onClick={this.handleFollowingClick}>FOLLOWING</button>
-        <br/><br/>
+        <div className="flw-btns">
+          <button onClick={this.handleFollowersClick} 
+                  className="followers-button"
+                  id="follower-button">FOLLOWERS</button> 
+          <button onClick={this.handleFollowingClick}
+                  className="following-button"
+                  id="following-button">FOLLOWING</button>
+        </div>
+        <br/>
         {this.state.followTabDefault && followers}
         {!this.state.followTabDefault && followees}
       </ul>
