24d49390969910e4eaec39cf9328a638968662b0
ndo277 <nhat.do277@gmail.com>
2019-07-08 17:12:43 -0700

    Clean up index container

--- frontend/components/following_index_container.js
+++ frontend/components/following_index_container.js
@@ -1,27 +1,16 @@
 import {connect} from 'react-redux';
 import FollowingIndex from './following_index';
-import {fetchFollows} from '../actions/follow_actions';
-import {fetchUsers} from '../actions/user_actions';
+import {createFollow, deleteFollow} from '../actions/follow_actions';
 
-const selectUsers = (users) => {
-  return Object.values(users);
-};
-
-const selectFollows = (follows) => {
-  return Object.values(follows);
-};
 
 const mapStateToProps = (state) => {
   const currentUser = state.entities.users[state.session.id];
-  const users = selectUsers(state.entities.users);
-  const follows = selectFollows(state.entities.follows);
-
-  return {currentUser, users, follows};
+  return {currentUser};
 }; 
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchFollows: () => dispatch(fetchFollows()),
-  fetchUsers: () => dispatch(fetchUsers())
+  createFollow: (data) => dispatch(createFollow(data)),
+  deleteFollow: (id) => dispatch(deleteFollow(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(FollowingIndex);
